157174578d50a7df6a26f36bb2db1e2a1663e5bf
L4ncelotz <laycxz123@gmail.com>
2024-09-14 00:13:18 +0700

    final 1.2.1

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
           />
         ) : (
           <img
-            src={"moodeng.jpg"}
+            src={"/assets/moodeng.jpg"}
             alt="หมูเด้ง"
             style={{ width: `${imageSize}px`, height: `${imageSize}px` }}
           />
